e924601a5f19cdeb750fb7849baf6dc56952c9b9
Christina <chris@example.com>
2026-01-20 12:49:55 +0530

    Add Google Analytics and updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import ReactGA from 'react-ga4'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
 import { About } from './components/About'
@@ -7,6 +9,13 @@ import { Contact } from './components/Contact'
 import { Footer } from './components/Footer'
 
 function App() {
+  useEffect(() => {
+    // TODO: Replace 'G-XXXXXXXXXX' with your actual Measurement ID from Google Analytics
+    // You can create a property at https://analytics.google.com/
+    ReactGA.initialize('G-RVCV154XZ9')
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
+  }, [])
+
   return (
     <div className="min-h-screen bg-dark">
       <Navbar />
